3ceb4617ae355ef9fc53a59e9482a19c87ae75ab
tigrri <lena81_2003@mail.ru>
2015-10-01 20:21:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 var headUrl = 'http://tigrri.github.io/ajax2/';
 var htmlList = ['header.html', 'section.html'];
 
-var someDeffered = $.ajax(headUrl + htmlList[0]);
-	someDeffered.then(function(result){
-	    $('.wrapper').append(result);
-	    return $.ajax(headUrl + htmlList[1])
-	}).then(function(result){
-	    $('.wrapper').append(result);
-	    return $.ajax(headUrl + htmlList[2])
-	});
+// var someDeffered = $.ajax(headUrl + htmlList[0]);
+// 	someDeffered.then(function(result){
+// 	    $('.wrapper').append(result);
+// 	    return $.ajax(headUrl + htmlList[1])
+// 	}).then(function(result){
+// 	    $('.wrapper').append(result);
+// 	    return $.ajax(headUrl + htmlList[2])
+// 	});
+
+
+animate = false;
+
+
+$.ajax({
+    url: 'http://tigrri.github.io/ajax2/header.html',
+  success: function(data) {
+    $('.wrapper').append(data);
+    animate = true;
+    // compare();
+  },
+  error: function(data) {
+    console.error(data);
+  }
+});
