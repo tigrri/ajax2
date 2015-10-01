ee9c50c93bd0521575da8b9795792da3df8982ba
tigrri <lena81_2003@mail.ru>
2015-10-01 20:34:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,6 @@
 var headUrl = 'http://tigrri.github.io/ajax2/';
 var htmlList = ['header.html', 'section.html'];
 
-// var someDeffered = $.ajax(headUrl + htmlList[0]);
-// 	someDeffered.then(function(result){
-// 	    $('.wrapper').append(result);
-// 	    return $.ajax(headUrl + htmlList[1])
-// 	}).then(function(result){
-// 	    $('.wrapper').append(result);
-// 	    return $.ajax(headUrl + htmlList[2])
-// 	});
 
 
 animate = false;
