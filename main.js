6c6e1fcd23dd112112a1fcc4c39759bdf3021520
tigrri <lena81_2003@mail.ru>
2015-10-01 20:29:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+Skip to content
+This repository  
+Search
+Pull requests
+Issues
+Gist
+ @tigrri
+ Unwatch 1
+  Star 0
+ Fork 0 tigrri/ajax2
+Branch: gh-pages  ajax2/main.js
+@tigrritigrri 2 minutes ago Update main.js
+1 contributor
+RawBlameHistory     45 lines (40 sloc)  942 Bytes
 var headUrl = 'http://tigrri.github.io/ajax2/';
 var htmlList = ['header.html', 'section.html'];
 
@@ -15,7 +29,9 @@ animate = false;
 
 var compare = function() {
 	if(animate && animate_2){
-		console.log('1');
+		console.log('ok');
+		$('.section').animate({left: "220px"}, 400);
+		$('.header').animate({left: "220px"}, 400);
 	}
 }
 $.ajax({
@@ -42,3 +58,5 @@ $.ajax({
     console.error(data);
   }
 });
+Status API Training Shop Blog About Pricing
+© 2015 GitHub, Inc. Terms Privacy Security Contact Help
