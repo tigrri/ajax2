4c8ce2351be78420eff3ec52a261f9c4adc6eece
tigrri <lena81_2003@mail.ru>
2015-10-01 19:59:50 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+var headUrl = 'http://bi0morph.github.io/ajax2/';
+var htmlList = ['header.html', 'section.html'];
+
+var someDeffered = $.ajax(headUrl + htmlList[0]);
+	someDeffered.then(function(result){
+	    $('.wrapper').append(result);
+	    return $.ajax(headUrl + htmlList[1])
+	}).then(function(result){
+	    console.log(JSON.stringify(result));
+	    $('.wrapper').append(result);
+	    return $.ajax(headUrl + htmlList[2])
+	}).then(function(result) {
+		$('.wrapper').append(result);
+	});
